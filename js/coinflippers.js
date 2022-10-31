8e4cb7a2dbb95b13b419fb6e01855cb8ceb69b2a
C0PK111r <116739981+C0PK111r@users.noreply.github.com>
2022-10-31 11:08:14 -0500

    Update coinflippers.js

--- js/coinflippers.js
+++ js/coinflippers.js
@@ -6,7 +6,7 @@ class CoinflipperManager {
         this._containerElement = document.getElementById(containerID);
         this._numberOfCoinflippers = 100;
         this._coinFlipperCost = 100;
-        this._coinFlipperCostMult = 1.1;
+        this._coinFlipperCostMult = 1;
         this._totalHeadsFlipped = 0;
         this._totalTailsFlipped = 0;
         this._totalSidesFlipped = 0;
